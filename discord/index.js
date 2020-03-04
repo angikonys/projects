6b8f66c6e714c3521c31669f133b7ded1fe9dce8
angiko <angikonys@gmail.com>
2020-03-04 20:43:58 +0100

    aled sherlox

--- discord/index.js
+++ discord/index.js
@@ -1,2 +1,29 @@
-const discord = require('discord.js');
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+const config = require("./config");
+
+const token = config.discord.token;
+
+const commandHandler = require('./commandHandler')
+
+const prefix = '!'
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	
+	
+
+	
+	
+});
+
+bot.login(token);
+
+
+
 
--- discord/migrations/202001021736-links-table.js
+++ discord/migrations/202001021736-links-table.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
         table.increments('id');
         table.text('url');
         table.text('title');
+        table.text('aliases')
         table.int('year');
         table.text('tags');
 
